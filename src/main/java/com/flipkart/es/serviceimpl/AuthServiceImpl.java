69e4b4474369b3769ab9e3f2f8bf0d263f44723a
mnnikhil111 <mnnikhil92@gmail.com>
2024-02-12 09:56:17 +0530

    Code Updated for User login

--- src/main/java/com/flipkart/es/serviceimpl/AuthServiceImpl.java
+++ src/main/java/com/flipkart/es/serviceimpl/AuthServiceImpl.java
@@ -1,19 +1,27 @@
 package com.flipkart.es.serviceimpl;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.flipkart.es.cache.CacheStore;
+import com.flipkart.es.entity.AccessToken;
 import com.flipkart.es.entity.Customer;
+import com.flipkart.es.entity.RefreshToken;
 import com.flipkart.es.entity.Seller;
 import com.flipkart.es.entity.User;
 import com.flipkart.es.enums.UserRole;
@@ -22,25 +30,35 @@ import com.flipkart.es.exception.InvalidUserRoleException;
 import com.flipkart.es.exception.OTPExpiredException;
 import com.flipkart.es.exception.RegistrationSessionExpiredException;
 import com.flipkart.es.exception.UserRegisteredException;
+import com.flipkart.es.repository.AccessTokenRepo;
 import com.flipkart.es.repository.CustomerRepository;
+import com.flipkart.es.repository.RefreshTokenRepo;
 import com.flipkart.es.repository.SellerRepository;
 import com.flipkart.es.repository.UserRepository;
+import com.flipkart.es.requestdto.AuthRequest;
 import com.flipkart.es.requestdto.OtpModel;
 import com.flipkart.es.requestdto.UserRequest;
+import com.flipkart.es.responsedto.AuthResponse;
 import com.flipkart.es.responsedto.UserResponse;
+import com.flipkart.es.security.JwtService;
 import com.flipkart.es.service.AuthService;
+import com.flipkart.es.util.CookieManager;
 import com.flipkart.es.util.MessageStructure;
 import com.flipkart.es.util.ResponseEntityProxy;
 import com.flipkart.es.util.ResponseStructure;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import com.flipkart.es.responsedto.*;
+import com.flipkart.es.requestdto.*;
 
 @Slf4j
 @Service
-@AllArgsConstructor
+
 public class AuthServiceImpl implements AuthService {
 
 	private UserRepository userRepository;
@@ -50,6 +68,39 @@ public class AuthServiceImpl implements AuthService {
 	private CacheStore<String> otpCacheStore;
 	private CacheStore<User> userCacheStore;
 	private JavaMailSender javaMailSender;
+	private AuthenticationManager authenticationManager;
+	private CookieManager cookieManager;
+	private JwtService jwtService;
+	private AccessTokenRepo accessTokenRepo;
+	private RefreshTokenRepo refreshTokenRepo;
+
+
+	@Value("${myapp.access.expiry}")
+	private int accessExpiryInSeconds;
+
+	@Value("${myapp.refresh.expiry}")
+	private int refreshExpiryInSeconds;
+
+
+
+	public AuthServiceImpl(UserRepository userRepository, SellerRepository sellerRepository,
+			CustomerRepository customerRepository, PasswordEncoder passwordEncoder, CacheStore<String> otpCacheStore,
+			CacheStore<User> userCacheStore, JavaMailSender javaMailSender, AuthenticationManager authenticationManager,
+			CookieManager cookieManager,JwtService jwtService,AccessTokenRepo accessTokenRepo,RefreshTokenRepo refreshTokenRepo) {
+		super();
+		this.userRepository = userRepository;
+		this.sellerRepository = sellerRepository;
+		this.customerRepository = customerRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.otpCacheStore = otpCacheStore;
+		this.userCacheStore = userCacheStore;
+		this.javaMailSender = javaMailSender;
+		this.authenticationManager = authenticationManager;
+		this.cookieManager = cookieManager;
+		this.jwtService=jwtService;
+		this.accessTokenRepo=accessTokenRepo;
+		this.refreshTokenRepo=refreshTokenRepo;
+	}
 
 	public User saveUser(User user) {
 
@@ -69,13 +120,13 @@ public class AuthServiceImpl implements AuthService {
 
 		User user = null;
 		switch (UserRole.valueOf(userRequest.getUserRole().toUpperCase())) {
-			case SELLER -> {
-				user = new Seller();
-			}
-			case CUSTOMER -> {
-				user = new Customer();
-			}
-			default -> throw new InvalidUserRoleException("User not found with the specified role");
+		case SELLER -> {
+			user = new Seller();
+		}
+		case CUSTOMER -> {
+			user = new Customer();
+		}
+		default -> throw new InvalidUserRoleException("User not found with the specified role");
 		}
 
 		user.setUsername(userRequest.getUserEmail().split("@")[0].toString());
@@ -101,45 +152,57 @@ public class AuthServiceImpl implements AuthService {
 				.build();
 	}
 
+	private AuthResponse mapToAuthResponse(User user)
+	{
+		return AuthResponse.builder()
+				.userId(user.getUserId())
+				.username(user.getUsername())
+				.Role(user.getUserRole().name())
+				.isAuthenticated(true)
+				.accessExpirationInSeconds(LocalDateTime.now().plusSeconds(accessExpiryInSeconds))
+				.refreshExpirationInSeconds(LocalDateTime.now().plusSeconds(refreshExpiryInSeconds))
+				.build();
+	}
+
 	private String generateOTP() {
 		return String.valueOf(new Random().nextInt(111111, 999999));
 	}
-	
+
 	private void sendOtpToMail(User user, String otp) throws MessagingException {
 		sendMail(MessageStructure.builder()
-		.to(user.getUserEmail())
-		.subject("complete your registration to flipkart electronics")
-		.sentDate(new Date())
-		.text(
-				"Hey " + user.getUsername()
-				+ " Welcome to flipkart electronics, <br>"
-				+ "Complete your registration using the OTP <br>"
-				+ "<h1><strong> "+otp+" </strong></h1> <br>"
+				.to(user.getUserEmail())
+				.subject("complete your registration to flipkart electronics")
+				.sentDate(new Date())
+				.text(
+						"Hey " + user.getUsername()
+						+ " Welcome to flipkart electronics, <br>"
+						+ "Complete your registration using the OTP <br>"
+						+ "<h1><strong> "+otp+" </strong></h1> <br>"
 						+ "<br><br>"
 						+ "Do not share this OTP with anyone"
 						+ "<br><br>"
 						+ "with best regards"
 						+ "FlipKart electronics"
 						)
-		.build());
+				.build());
 	}
 
 	public void sendWelcomeMessage(User user) throws MessagingException{
 		sendMail(MessageStructure.builder()
-		.to(user.getUserEmail())
-		.subject("Welcome to flipkart electronics")
-		.sentDate(new Date())
-		.text(
-			"Hello " + user.getUsername()
-			+ " we welcome you to flipkart electronics <br>"
-			+ " we at flipkart do not call our customers for the password or band related issues <br><br>"
-			+ " Beware of fraudsters"
-			+ " Have a nice shopping"
-			+ " with best regards"
-			+ " Flipkart electronics"
-		).build());
-		
-		
+				.to(user.getUserEmail())
+				.subject("Welcome to flipkart electronics")
+				.sentDate(new Date())
+				.text(
+						"Hello " + user.getUsername()
+						+ " we welcome you to flipkart electronics <br>"
+						+ " we at flipkart do not call our customers for the password or band related issues <br><br>"
+						+ " Beware of fraudsters"
+						+ " Have a nice shopping"
+						+ " with best regards"
+						+ " Flipkart electronics"
+						).build());
+
+
 	}
 
 	@Async
@@ -152,7 +215,7 @@ public class AuthServiceImpl implements AuthService {
 		helper.setText(messageStructure.getText(), true);
 		javaMailSender.send(mimeMessage);
 	}
-	
+
 	@Override
 	public ResponseEntity<ResponseStructure<UserResponse>> registerUser(UserRequest userRequest) {
 
@@ -208,6 +271,52 @@ public class AuthServiceImpl implements AuthService {
 
 		return ResponseEntityProxy.setResponseStructure(HttpStatus.CREATED,
 				"user registered successfully", mapToUserResponse(user));
+
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<AuthResponse>> login(AuthRequest authRequest,HttpServletResponse httpServletResponse) {
+
+		String username= authRequest.getUserEmail().split("@")[0];
+		UsernamePasswordAuthenticationToken token=new UsernamePasswordAuthenticationToken(username, authRequest.getUserPassword());
+		Authentication authentication = authenticationManager.authenticate(token);
+		if(!authentication.isAuthenticated())
+			throw new UsernameNotFoundException("Failed to authenticate the user");
+		
+		return userRepository.findByUsername(username).map(user -> {
+			grantAccess(httpServletResponse, user);
+			
+			return ResponseEntityProxy.setResponseStructure(HttpStatus.OK, "success", mapToAuthResponse(user));
+		})
+		.orElseThrow(() -> new UsernameNotFoundException("User name not found"));
+
+
+
+		
+
+	}
+
+	private void grantAccess(HttpServletResponse response,User user)
+	{
+		String accessToken = jwtService.generateAccessToken(user.getUsername());
+		String refreshToken = jwtService.generateRefreshToken(user.getUsername());
+
+
+		response.addCookie(cookieManager.configure(new Cookie("at", accessToken),accessExpiryInSeconds));			
+		response.addCookie(cookieManager.configure(new Cookie("rt", refreshToken),refreshExpiryInSeconds));
+
+		accessTokenRepo.save(AccessToken.builder()
+				.token(accessToken)
+				.isBlocked(false)
+				.expiration(LocalDateTime.now().plusSeconds(accessExpiryInSeconds))
+				.build());
+
+		refreshTokenRepo.save(RefreshToken.builder()
+				.token(refreshToken)
+				.isBlocked(false)
+				.expiration(LocalDateTime.now().plusSeconds(refreshExpiryInSeconds))
+				.build());
+
 
 	}
 
